73737577fbf679e5c847033f819135ee680a2d26
Troray <troraycom@gmail.com>
2025-12-28 22:18:47 +0800

    Update SkipController.tsx

--- src/components/SkipController.tsx
+++ src/components/SkipController.tsx
@@ -634,7 +634,7 @@ export default function SkipController({
                       type="text"
                       value={batchSettings.openingEnd}
                       onChange={(e) => setBatchSettings({...batchSettings, openingEnd: e.target.value})}
-                      className="w-[95px] px-3 py-2 border border-gray-300 dark:border-gray-600 rounded bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 text-center"
+                      className="w-[166px] px-3 py-2 border border-gray-300 dark:border-gray-600 rounded bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 text-center"
                       placeholder="1:30"
                     />
                   </div>
